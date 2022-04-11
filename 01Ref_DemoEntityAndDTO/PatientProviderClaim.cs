32ad82dc29dda2131dd7ee03368d4cf372217f69
vinayak.sawant <vinayaksawant@hotmail.com>
2022-04-10 19:11:55 -0500

    Started working on API to Cosmos

---
+++ 01Ref_DemoEntityAndDTO/PatientProviderClaim.cs
@@ -0,0 +1,101 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoEntityAndDTO
+{
+    public class PatientProviderClaim
+    {
+        [JsonProperty("BeneID")]
+        public string BeneID { get; set; }
+
+        [JsonProperty("ClaimID")]
+        public string ClaimID { get; set; }
+
+        [JsonProperty("ClaimStartDt")]
+        public string ClaimStartDt { get; set; }
+
+        [JsonProperty("ClaimEndDt")]
+        public string ClaimEndDt { get; set; }
+
+        [JsonProperty("Provider")]
+        public string Provider { get; set; }
+
+        [JsonProperty("InscClaimAmtReimbursed")]
+        public string InscClaimAmtReimbursed { get; set; }
+        
+        [JsonProperty("AttendingPhysician")]
+        public string AttendingPhysician { get; set; }
+        
+        [JsonProperty("OperatingPhysician")]
+        public string OperatingPhysician { get; set; }
+
+        [JsonProperty("OtherPhysician")]
+        public string OtherPhysician { get; set; }
+
+        [JsonProperty("AdmissionDt")]
+        public string AdmissionDt { get; set; }
+
+        [JsonProperty("ClmAdmitDiagnosisCode")]
+        public string ClmAdmitDiagnosisCode { get; set; }
+
+        [JsonProperty("DeductibleAmtPaid")]
+        public string DeductibleAmtPaid { get; set; }
+
+        [JsonProperty("DiagnosisGroupCode")]
+        public string DiagnosisGroupCode { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_1")]
+        public string ClmDiagnosisCode_1 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_2")]
+        public string ClmDiagnosisCode_2 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_3")]
+        public string ClmDiagnosisCode_3 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_4")]
+        public string ClmDiagnosisCode_4 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_5")]
+        public string ClmDiagnosisCode_5 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_6")]
+        public string ClmDiagnosisCode_6 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_7")]
+        public string ClmDiagnosisCode_7 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_8")]
+        public string ClmDiagnosisCode_8 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_9")]
+        public string ClmDiagnosisCode_9 { get; set; }
+
+        [JsonProperty("ClmDiagnosisCode_10")]
+        public string ClmDiagnosisCode_10 { get; set; }
+
+        [JsonProperty("ClmProcedureCode_1")]
+        public string ClmProcedureCode_1 { get; set; }
+
+        [JsonProperty("ClmProcedureCode_2")]
+        public string ClmProcedureCode_2 { get; set; }
+
+        [JsonProperty("ClmProcedureCode_3")]
+        public string ClmProcedureCode_3 { get; set; }
+
+        [JsonProperty("ClmProcedureCode_4")]
+        public string ClmProcedureCode_4 { get; set; }
+
+        [JsonProperty("ClmProcedureCode_5")]
+        public string ClmProcedureCode_5 { get; set; }
+
+        [JsonProperty("ClmProcedureCode_6")]
+        public string ClmProcedureCode_6 { get; set; }
+
+
+    }
+}
+
+
---
+++ Demo_OnPremAPI_ToCosmos/Controllers/PatientProviderClaimController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DemoEntityAndDTO;
+using Microsoft.Azure.Cosmos;
+
+namespace Demo_OnPremAPI_ToCosmos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientProviderClaimController : ControllerBase
+    {
+        // The Azure Cosmos DB endpoint for running this sample.
+        private static readonly string EndpointUri = "https://vinayakcosmosdb.documents.azure.com:443/";
+
+        // The primary key for the Azure Cosmos account.
+        private static readonly string PrimaryKey = "HDxdUMmhEB2d53nFNmHVCAqOnDCBkn5u8K9y8HY1Sjp2R9gdJE8i3WjVGmuFLW27Gw5oqnzywW5r2coRQWeXfg==";
+
+        // The Cosmos client instance
+        private CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        // The name of the database and container we will create
+        private string databaseId = "CoffeeJoint_ClientProvider_Data";
+        private string containerId = "OrangeBoosterProviders1";
+
+        // GET: api/PatientProviderClaim
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/PatientProviderClaim/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/PatientProviderClaim
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/PatientProviderClaim/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
